74f95984bf93e6eb3ed78bc6a738fda9f9530d98
itshariharan <148336675+itshariharan@users.noreply.github.com>
2023-11-30 12:21:04 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className='text-[49px]'>
+        <p className='text-[49px] font-semibold'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
